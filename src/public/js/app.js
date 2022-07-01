08ba2080a317f11933699c290aa4ab0f06337f12
cyjinstar <cyjinstar@naver.com>
2022-07-02 00:20:09 +0900

    WebSocketVersion_0.1.0

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,39 @@
-alert("JS__Check__Success!");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const IDForm = document.querySelector("#id");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload){
+    const msg = {type, payload };
+    return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+    console.log("Connect is Stable!👍");
+});
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnect from Sever.");
+});
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleIDSubmit(event){
+    event.preventDefault();
+    const input = IDForm.querySelector("input");
+    socket.send(makeMessage("id", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+IDForm.addEventListener("submit", handleIDSubmit);
--- src/server.js
+++ src/server.js
@@ -16,4 +16,37 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["id"] = "Undefied"
+    console.log("Connected from the Browser.");
+    socket.on("close", () => console.log("Disconnected from the Browser."));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => 
+                aSocket.send(`${socket.id}: ${message.payload}`));
+                break;
+            case "id" : 
+                socket["id"] = message.payload;
+                if(message.payload === ""){
+                    socket["id"] = "Undefied"
+                }
+                break;
+        }
+    });
+});
+
 server.listen(3000, handleListen);
+
+{
+    type:"message";
+    payload:"hello";
+}
+{
+    type:"id";
+    payload:"hello";
+}
